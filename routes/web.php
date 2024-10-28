d6c8a8f0096626676da4e81a6f67bb5e8c02a1f4
VoidEren <voiderent@gmail.com>
2024-10-27 23:26:55 -0600

    Vistas y Tablas

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
+use App\Http\Controllers\PizzaController;
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -19,7 +19,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/pizzas',[PizzaController::class, 'index'])->name('pizzas.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/pizzas{pizza}', [PizzaController::class, 'edit'])->name('pizzas.edit');
+    Route::patch('/pizzas{pizza}', [PizzaController::class, 'update'])->name('pizzas.update');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
